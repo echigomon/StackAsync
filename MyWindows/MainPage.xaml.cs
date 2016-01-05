ff3a333864925df52ed9aa63e44b7c6ade0f8b87
smatsuda7350 <smatsuda7350@gmail.com>
2016-01-05 23:20:16 +0900

    初期設定１

---
+++ MyWindows/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using LRSkipAsync;
+using StackAsync;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace MyWindows
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button01_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button02_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button03_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button04_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button05_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button06_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button07_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button08_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button09_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
